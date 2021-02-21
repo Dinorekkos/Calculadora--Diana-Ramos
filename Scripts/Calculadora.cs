c1f3b25189cad65bff60cb853456c0d0ddefd7fe
Diana Ramos <dianarg574@gmail.com>
2021-02-21 14:17:25 -0600

    Creacion Escena
    Meter todos los arch en un mismo arch git

--- Scripts/Calculadora.cs
+++ Scripts/Calculadora.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Calculadora : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   
+
+
+
+    public void Sumar()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public void ShowInput()
     {
-        
+
     }
 }
